c9c6af8498dc518a8d865a89c2d833aea5669195
lbilinmis <leventamed@gmail.com>
2023-04-22 12:30:40 +0300

    Dbset Methodları

--- EFCORE6.CodeFirst/Program.cs
+++ EFCORE6.CodeFirst/Program.cs
@@ -2,6 +2,8 @@
 using EFCORE6.CodeFirst.DAL;
 using EFCORE6.CodeFirst.Models;
 using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
 
 Console.WriteLine("Hello, World!");
 
@@ -9,42 +11,25 @@ Console.WriteLine("Hello, World!");
 //using (var context = new AppDbContext(DbContextInitializer.OptionsBuilder.Options))
 using (var context = new AppDbContext())
 {
-    //// db ye veri ekleme işlemi hazırlığında state durumu detached durumda olur 
-    //// ekleme işlemi gerçekleştikten sonra state durumu added durumda olur
-    //var product = new Product() { Name="Yeni Ürün 1",Barcode="Barcode Yeni 1",Price=100,Stock=110 };
-
-    //Console.WriteLine(" Ekleme yapmadan state durumu : "+context.Entry(product).State);
-    //context.Entry(product).State = EntityState.Added;
-    ////await context.Products.AddAsync(product);
-    //Console.WriteLine(" Ekleme yapıldıktan sonra state durumu : " + context.Entry(product).State);
-    //await context.SaveChangesAsync();
-    //Console.WriteLine(" Save change yapıldıktan sonra state durumu : " + context.Entry(product).State);
+    //// First methodu ile kayıt bulunamadığı taktirde exception fırlatılır
+    //var products=context.Products.First(x=>x.Id==18);
 
 
-    //var products = await context.Products.ToListAsync();
-    var productsAsNoTracking = await context.Products.AsNoTracking().ToListAsync();
-
-    productsAsNoTracking.ForEach(p =>
-    {
-        // db den veri çekildiğinde ilk state durumu Unchange olur
-        var state = context.Entry(p).State;
-        Console.WriteLine(p.Id + " : Product : " + p.Name + " Price : " + p.Price + " State " + state);
-    });
+    // //FirstOrDefault methodu ile kayıt bulunamadığı taktirde null döner
+    //var products = context.Products.FirstOrDefault(x => x.Id == 18);
 
 
-    // aşağıdaki kod bloğu memory projede track edilen datalar üzerinde işlem yapıyor
-    context.ChangeTracker.Entries().ToList().ForEach(x =>
-    {
-        if (x.Entity is Product p)
-        {
-            //p.Price += 100;
-            Console.WriteLine(p.Id + " : Product : " + p.Name + " Price : " + p.Price );
+    ////Single methodu ile eşleşen birden fazla kayıt bulunduğun taktirde excepiton döner
+    //var products = context.Products.Single(x => x.Id <= 18);
 
-        }
-    });
 
-    context.SaveChangesAsync();
+    ////Where methodu ile eşleşen bir kayıt bulunmadığı taktirde null döner
+    //var products = context.Products.Where(x => x.Id <= 18);
 
-    // asnotracking sql server tarafında gelen datanın EFCORE tarafındna track edilmemesini sağlar
-    // memory de tutulmaz ram i daha düzgün kullanır
+
+    //Find methodu id ile arama işlemlerinde kullanılır tek bir kayıt için kullanılmaktadır
+    // bulamadığı taktirde null dönecektir hata forlatmasını istersek single kullanılır
+    var products = context.Products.Find(17);
+
+    Console.WriteLine( products.Barcode);
 }
